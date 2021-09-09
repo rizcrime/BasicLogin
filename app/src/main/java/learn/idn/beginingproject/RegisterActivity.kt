6ebc4acbd97aaea3318bd3d8fc85b78329f779e9
mr_reyzq <rizkiafh180@gmail.com>
2021-09-09 09:06:47 +0700

    new Register version

---
+++ app/src/main/java/learn/idn/beginingproject/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package learn.idn.beginingproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
